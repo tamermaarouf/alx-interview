3692080b08fbe4616fe61c708d90108da9bd2666
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-26 05:15:38 +0200

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 '''
-Write a method that determines if a given data set represents a valid UTF-8 encoding.
+Write a method that determines
+if a given data set represents a valid UTF-8 encoding.
 
 Prototype: def validUTF8(data)
 Return: True if data is a valid UTF-8 encoding, else return False
 A character in UTF-8 can be 1 to 4 bytes long
 The data set can contain multiple characters
 The data will be represented by a list of integers
-Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
+Each integer represents 1 byte of data,
+therefore you only need to handle the 8 least significant bits of each integer
 '''
 
 
 def validUTF8(data):
+    '''
     binary_data = []
     for dt in data:
         binary_data.append(dec2bin(dt))
     return binary_data
-    pass
-
-def dec2bin(number: int):
-    ans = ''
-    if ( number == 0 ):
-        return 0
-    while ( number ):
-        ans += str(number&1)
-        number = number >> 1
-
-    ans = ans[::-1].zfill(8)
-    print(f'---{ans}')
-    return ans
+    '''
+    count = 0
+    for binary_data in data:
+        if (count == 0):
+            if ((binary_data >> 5) == 110):
+                count = 1
+            elif ((binary_data >> 4) == 1110):
+                count = 2
+            elif ((binary_data >> 3) == 11110):
+                count = 3
+            elif ((binary_data >> 7) != 0):
+                return False
+        else:
+            if ((binary_data >> 6) != 10):
+                return False
+            count -= 1
+    return (count == 0)
