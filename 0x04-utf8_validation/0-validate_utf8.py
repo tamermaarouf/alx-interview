ac4ba5112a5641539d129c6944e23590a39f55a8
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-26 13:18:19 +0200

    valid UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,7 +23,7 @@ def validUTF8(data):
                 count -= 1
             else:
                 return False
-        elif count == 0:
+        else:
             if ((binary_data >> 7) == 0):
                 count = 0
             elif ((binary_data >> 5) == 0b110):
