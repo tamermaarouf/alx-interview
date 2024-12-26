78bc05e4b571aa7681ba4fd3c753020e7c7b9f89
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-26 14:24:51 +0200

    valid UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -35,6 +35,4 @@ def validUTF8(data):
                 count = 2
             elif ((binary_data >> 3) == 0b1110):
                 count = 3
-            else:
-                return False
     return (count == 0)
